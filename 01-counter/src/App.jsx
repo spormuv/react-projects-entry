eed552aa56acb9c2746fd52525b2df206e491036
Mike <spormuv@gmail.com>
2023-07-04 18:53:09 +0300

    feat: add counter

---
+++ 01-counter/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const onClickPlus = () => {
+    setCount(count + 1);
+  };
+
+  const onClickMinus = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <h2>Counter:</h2>
+        <h1>{count}</h1>
+        <button className="minus" onClick={onClickMinus}>
+          - Minus
+        </button>
+        <button className="plus" onClick={onClickPlus}>
+          Plus +
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
