fe6f618c30bd0f2fff7572079eb8506e33392069
Mike <spormuv@gmail.com>
2023-07-04 19:36:51 +0300

    feat: add modal

--- 02-modal/src/App.jsx
+++ 02-modal/src/App.jsx
@@ -1,5 +1,20 @@
+import { useState } from 'react';
+import Modal from './components/Modal';
+
 function App() {
-  return <div></div>;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="App">
+      <button className="open-modal-btn" onClick={() => setOpen(true)}>
+        ✨ Open Modal
+      </button>
+
+      {/* {open && <Modal setOpen={setOpen} open={open} />} */}
+
+      <Modal setOpen={setOpen} open={open} />
+    </div>
+  );
 }
 
 export default App;
