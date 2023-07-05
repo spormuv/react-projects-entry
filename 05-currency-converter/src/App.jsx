2e015c7de1b3eae58590945addb32bfcf6454d02
Mike <spormuv@gmail.com>
2023-07-05 19:38:08 +0300

    feat: add currency converter

--- 05-currency-converter/src/App.jsx
+++ 05-currency-converter/src/App.jsx
@@ -1,5 +1,58 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useRef, useState } from 'react';
+import Block from './components/Block';
+
 function App() {
-  return <div></div>;
+  const [currencyFrom, setCurrencyFrom] = useState('USD');
+  const [currencyTo, setCurrencyTo] = useState('EUR');
+  const [amountFrom, setAmountFrom] = useState(0);
+  const [amountTo, setAmountTo] = useState(0);
+  const ratesRef = useRef({});
+
+  useEffect(() => {
+    fetch('https://api.exchangerate.host/latest?base=USD')
+      .then(res => res.json())
+      .then(data => (ratesRef.current = data.rates))
+      .catch(err => console.error(err));
+  }, []);
+
+  const onChangeAmountFrom = value => {
+    const rawValue = value / ratesRef.current[currencyFrom];
+    const result = rawValue * ratesRef.current[currencyTo];
+    setAmountFrom(value);
+    setAmountTo(result.toFixed(2));
+  };
+  const onChangeAmountTo = value => {
+    const result =
+      (ratesRef.current[currencyFrom] / ratesRef.current[currencyTo]) * value;
+    setAmountFrom(result.toFixed(2));
+    setAmountTo(value);
+  };
+
+  useEffect(() => {
+    onChangeAmountFrom(amountFrom);
+  }, [currencyFrom]);
+
+  useEffect(() => {
+    onChangeAmountTo(amountTo);
+  }, [currencyTo]);
+
+  return (
+    <div className="App">
+      <Block
+        value={amountFrom}
+        currency={currencyFrom}
+        onChangeCurrency={setCurrencyFrom}
+        onChangeAmount={onChangeAmountFrom}
+      />
+      <Block
+        value={amountTo}
+        currency={currencyTo}
+        onChangeCurrency={setCurrencyTo}
+        onChangeAmount={onChangeAmountTo}
+      />
+    </div>
+  );
 }
 
 export default App;
